cb5f4194c193d49858004a06cbebd29123c65fd6
git-justinpodur <git-justin@podur.org>
2014-06-06 14:47:53 -0400

    added rolodex and contact classes

--- crm.rb
+++ crm.rb
@@ -46,8 +46,43 @@ class CRM
 		end
 	end
 
+	class Contact
+		def initialize(first_name,last_name,email,note)
+			@first_name=first_name
+			@last_name=last_name
+			@email=email
+			@note=note
+		end
+
+		attr_accessor :first_name,:last_name,:email,:note
+
+	end
+
+	class Rolodex
+		def initialize
+			@contacts = []
+			@id = 1000
+		end
+		
+		attr_reader :contacts
+
+		def add_contact(contact)
+			contact.id=@id
+			@contacts << contact
+		end
+	end
+
+
 	def add_new_contact
-		puts "adding new contact"
+		puts "Enter First Name: "
+		first_name=gets.chomp
+		print "Enter Last Name: "
+		last_name=gets.chomp
+		print "Enter Email Address: "
+		email=gets.chomp
+		print "Enter note: "
+		note=gets.chomp
+		contact=Contact.new(first_name,last_name,email,note)
 	end
 
 	def modify_existing_contact
