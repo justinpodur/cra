79f23236f13b5bcc16281e15e5c8cb5f494ba6b8
git-justinpodur <git-justin@podur.org>
2014-06-06 10:44:00 -0400

    set up some of the runner

---
+++ crm.rb
@@ -0,0 +1,47 @@
+def print_main_menu
+	puts "[1] Add new contact"
+	puts "[2] Modify an existing contact"
+	puts "[3] Display all contacts"
+	puts "[4] Display a contact"
+	puts "[5] Display an attribute of a contact"
+	puts "[6] Delete a contact"
+	puts "[7] Exit"
+	puts "Enter a number: "
+end
+
+def main_menu
+	print_main_menu
+	user_selected = gets.to_i
+	call_option(user_selected)
+end
+
+def call_option(user_selected)
+	add_new_contact if user_selected == 1
+	modify_existing_contact if user_selected == 2
+	display_all_contacts if user_selected == 3
+	display_contact if user_selected == 4
+	display_attribute if user_selected == 5
+	delete_contact if user_selected == 6
+	exit if user_selected == 7
+end
+
+def add_new_contact
+end
+
+def modify_existing_contact
+end
+
+def display_all_contacts
+end
+
+def display_contact
+end
+
+def display_attribute
+end
+
+def delete_contact
+end
+
+def exit
+end
